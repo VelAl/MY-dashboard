8d05dcac2017ce9f65d76366feccfcf19a3ec980
Alex <welmozhko88@gmail.com>
2025-05-13 11:22:24 +0300

    Added hero images

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { ArrowRightIcon } from "@heroicons/react/24/outline";
 import clsx from "clsx";
 import Link from "next/link";
 import { lusitana } from "./ui/fonts";
+import Image from "next/image";
 
 export default function Page() {
   return (
@@ -23,7 +24,7 @@ export default function Page() {
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
             </a>
-            , brought to you by Vercel.
+            , brought by Vercel.
           </p>
           <Link
             href="/login"
@@ -33,7 +34,22 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop version"
+            priority
+          />
+          <Image
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            className="md:hidden"
+            alt="Screenshots of the dashboard project showing mobile version"
+            priority
+          />
         </div>
       </div>
     </main>
